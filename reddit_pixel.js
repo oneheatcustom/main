0cd0a93ace87ca95e874de0266aa2eeaeca2d1ca
b_growe <130042961+oneheatcustom@users.noreply.github.com>
2026-02-09 14:49:53 +0200

    Update reddit_pixel.js

--- reddit_pixel.js
+++ reddit_pixel.js
@@ -1,4 +1,4 @@
-(function (w, d) {
+(function(w, d){
   if (w.__redditChecked) return;
   w.__redditChecked = true;
 
@@ -7,28 +7,52 @@
     return r ? decodeURIComponent(r[1]) : null;
   }
 
-  var redditId = getParam('raddit_id');
+  function setCookie(name, value, days) {
+    if (!value) return;
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+
+    var domainParts = location.hostname.split('.');
+    var domain = domainParts.length > 1 ? '.' + domainParts.slice(-2).join('.') : location.hostname;
+
+    document.cookie = name + "=" + value + expires + "; path=/; domain=" + domain;
+  }
+
+  function getCookie(name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    return match ? match[2] : null;
+  }
+
+  var redditId = getParam('raddit_id') || getCookie('raddit_id');
 
   if (redditId !== 'a2_ie30vgoc3goy') return;
 
-  if (w.__redditPixelInited) return;
-  w.__redditPixelInited = true;
+  // Сохраняем cookie на корневой домен
+  setCookie('raddit_id', redditId, 7);
 
-  !function(w,d){
-    if(!w.rdt){
-      var p=w.rdt=function(){
-        p.sendEvent ? p.sendEvent.apply(p,arguments) : p.callQueue.push(arguments);
-      };
-      p.callQueue=[];
-      var s=d.createElement("script");
-      s.src="https://www.redditstatic.com/ads/pixel.js";
-      s.async=true;
-      var e=d.getElementsByTagName("script")[0];
-      e.parentNode.insertBefore(s,e);
-    }
-  }(w,d);
+  if (!w.__redditPixelInited) {
+    w.__redditPixelInited = true;
 
-  rdt('init', redditId);
-  rdt('track', 'PageVisit');
+    !function(w,d){
+      if(!w.rdt){
+        var p=w.rdt=function(){
+          p.sendEvent ? p.sendEvent.apply(p,arguments) : p.callQueue.push(arguments);
+        };
+        p.callQueue=[];
+        var s=d.createElement("script");
+        s.src="https://www.redditstatic.com/ads/pixel.js";
+        s.async=true;
+        var e=d.getElementsByTagName("script")[0];
+        e.parentNode.insertBefore(s,e);
+      }
+    }(w,d);
+
+    rdt('init', redditId);
+    rdt('track','PageVisit');
+  }
 
 })(window, document);
